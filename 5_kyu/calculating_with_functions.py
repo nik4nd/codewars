ad4ce9b9136254996953c21f39e6d32f7ce1a8f7
nik4nd <nik4nd@gmail.com>
2022-04-14 23:56:39 +0300

    Add calculating_with_functions.py

---
+++ 5_kyu/calculating_with_functions.py
@@ -0,0 +1,14 @@
+def zero(func = False): return func(0) if func else 0
+def one(func = False): return func(1) if func else 1
+def two(func = False): return func(2) if func else 2
+def three(func = False): return func(3) if func else 3
+def four(func = False): return func(4) if func else 4
+def five(func = False): return func(5) if func else 5
+def six(func = False): return func(6) if func else 6
+def seven(func = False): return func(7) if func else 7
+def eight(func = False): return func(8) if func else 8
+def nine(func = False): return func(9) if func else 9
+def plus(b): return lambda a: a + b
+def minus(b): return lambda a: a - b
+def times(b): return lambda a: a * b
+def divided_by(b): return lambda a: a // b
